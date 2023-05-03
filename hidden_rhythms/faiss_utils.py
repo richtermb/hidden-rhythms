03f8ddfa59300cfcb47ea5f1255b9fa12db0fdec
richtermb <rmbrzeski@gmail.com>
2023-05-03 11:03:25 -0700

    get barebones faiss working

---
+++ hidden_rhythms/faiss_utils.py
@@ -0,0 +1,18 @@
+import faiss
+
+class FAISSWrapper(object):
+  def __init__(self, dims):
+    print("Initialized FAISS wrapper")
+    self.index = faiss.IndexFlatL2(dims)
+
+  def add(self, v):
+    self.index.add(v)
+
+  def set_nprobe(self, n):
+    self.index.nprobe = n
+
+  def search(self, v, n):
+    return self.index.search(v, n)
+
+  def get_ntotal(self):
+    return self.index.ntotal
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
 	zip_path = os.path.join(dirname, "../hidden_rhythms/tracks.csv.zip")
 	print("Testing dataset at {}...".format(zip_path))
 	df = dataset.load_tracks_dataset(zip_path)
-	print("Getting first row...")
-	print(dataset.vectorize(df.iloc[0]))
+	print("Getting first 10 rows...")
+	for i in range(10):
+		print(dataset.vectorize(df.iloc[i]))

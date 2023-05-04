2b4b45d8508e0263d7e2a6c8366672b433a337cb
richtermb <rmbrzeski@gmail.com>
2023-05-03 18:01:31 -0700

    add statistical tools in recommender

--- hidden_rhythms/recommender.py
+++ hidden_rhythms/recommender.py
@@ -1,18 +1,38 @@
 import numpy as np
 
 class Recommender(object):
-  def __init__(self, n_friends, n_songs):
+  def __init__(self, n_friends, songs_per_friend):
     self.n_friends = n_friends
-    self.n_songs = n_songs
+    self.songs_per_friend = songs_per_friend
     self.friend_count = 0
+    self.songs = []
 
-
-  def add_friend(song_prefs):
+  def add(self, song_prefs):
     # song_prefs is an np.array of the vectorized songs
-    assert(self.n_songs == np.shape(song_prefs)[0])
+    assert(self.songs_per_friend == np.shape(song_prefs)[0])
     # generate knns, etc.
+    for v in song_prefs:
+      self.songs.append(v)
+
+
     self.friend_count += 1
 
-  def generate_shared_dp_playlist():
+  def generate_shared_dp_playlist(self):
     assert(self.friend_count == self.n_friends)
     # run alg to generate np array of vectorized songs fufilling DP requirements
+
+  def compute_songs_mean(self):
+    a = np.array(self.songs)
+    return np.mean(a, axis=0)
+
+  def compute_songs_median(self):
+    a = np.array(self.songs)
+    median = np.median(a, axis=0)
+
+  def compute_songs_variance(self):
+    a = np.array(self.songs)
+    return np.var(a, axis=0)
+
+  def corrcoef(self):
+    a = np.array(self.songs)
+    return np.corrcoef(a, rowvar=False)
